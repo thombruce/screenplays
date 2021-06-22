c71871b075de32df4fa74bd6e5e4b865bff6607c
thombruce <thombruce@users.noreply.github.com>
2021-06-22 12:31:36 +0000

    deploy: c7de879a805500c56f3411634216b6535ccb8e67

---
+++ _nuxt/static/1624365091/screenplays/last-thursday/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624365091",layout:"default",error:a,serverRendered:b,routePath:"\u002Fscreenplays\u002Flast-thursday",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bb4d2bba"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
---
+++ _nuxt/static/1624365091/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624365091",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bb4d2bba"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
